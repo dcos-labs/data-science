f55ac56000ebd59cbf8fb52084014aaae0754c0b
Christopher <cgutierrez@mesosphere.io>
2019-04-25 16:25:01 -0700

    Spacing Change

--- analytic/sql/success_rate_per_category.sql
+++ analytic/sql/success_rate_per_category.sql
@@ -1,13 +1,13 @@
 -- Success Rate of Campaigns per Category
 SELECT main_category,
-       SUM(CASE
-               WHEN state = 'successful' THEN 1
-               ELSE 0
-           END) success_count,
-       SUM(CASE
-               WHEN state <> 'successful' THEN 1
-               ELSE 0
-           END) fail_count
+  SUM(CASE
+    WHEN state = 'successful' THEN 1
+    ELSE 0
+  END) success_count,
+  SUM(CASE
+    WHEN state <> 'successful' THEN 1
+    ELSE 0
+  END) fail_count
 FROM tx.kickstart k
 WHERE state NOT IN ('live',
                     'undefined')
